49ec85b37ccc4b095c2397f5f98a044d6edb920a
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-01-27 13:31:45 +0200

    add JS file

---
+++ app.js
@@ -0,0 +1,10 @@
+var countryName = prompt("Where are you from?")
+
+if (countryName == 'jordan') {
+    var yourName = prompt('What is your name?')
+    alert("welcome "+ yourName +" You're in the rigth place")
+} else {
+    alert("Sorry, we're just located in Jordan")
+}
+
+document.write('<b>',"Amman - Jordan", '</b>')
